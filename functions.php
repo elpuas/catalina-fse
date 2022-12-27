afc90668033bbc8d19e232fdd58fa54862eca783
Alfredo Navas <elpuas@gmail.com>
2022-12-27 09:55:34 -0600

    update pattern hero

--- functions.php
+++ functions.php
@@ -14,13 +14,13 @@
  * @author Alfredo Navas <elpuas@gmail.com>
  */
 function catalina_get_theme_include_files() {
-	return [
-		'inc/scripts.php', // Load styles and scripts.
-		'inc/patterns.php', // Gutenberg patterns for this theme.
-		'inc/block-admin.php', // Block Functions for this theme.
-	];
+    return [
+        'inc/scripts.php', // Load styles and scripts.
+        'inc/patterns.php', // Gutenberg patterns for this theme.
+        'inc/block-admin.php', // Block Functions for this theme.
+    ];
 }
 
 foreach ( catalina_get_theme_include_files() as $include ) {
-	require trailingslashit( get_template_directory() ) . $include;
+    include trailingslashit(get_template_directory()) . $include;
 }
--- patterns/hero.php
+++ patterns/hero.php
@@ -12,13 +12,13 @@
 <p class="has-secondary-color has-text-color has-section-content-font-size" style="text-transform:uppercase"><strong>Veritatis Obcaecati Tenetur</strong></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"}},"textColor":"secondary"} -->
-<h1 class="has-secondary-color has-text-color" style="text-transform:uppercase">I code digital crafts <strong>for </strong>humans.</h1>
+<!-- wp:heading {"level":1,"style":{"typography":{"textTransform":"uppercase"}},"textColor":"secondary"} -->
+<h1 class="wp-block-heading has-secondary-color has-text-color" style="text-transform:uppercase">I code digital crafts <strong>for </strong>humans.</h1>
 <!-- /wp:heading -->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"secondary","textColor":"primary"} -->
-<div class="wp-block-button"><a class="wp-block-button__link has-primary-color has-secondary-background-color has-text-color has-background">READ MORE</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link has-primary-color has-secondary-background-color has-text-color has-background wp-element-button">READ MORE</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></section>
 <!-- /wp:group -->
